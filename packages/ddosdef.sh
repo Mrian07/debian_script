dd0e0c4bad899b63844371da271400520bc5d82e
Doctype <87948372+cybertize@users.noreply.github.com>
2022-06-28 02:30:20 +0800

    Add files via upload

---
+++ packages/ddosdef.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source /usr/local/cybertize/helper/color.sh
+source /usr/local/cybertize/helper/function.sh
+source /usr/local/cybertize/helper/utility.sh
+
+clear && echo
+echo -e "${RED}=====================================================${CLR}"
+echo -e "── █ █▀▀▀█ █▀▀▀ █─▄▀ █▀▀▀ █▀▀█ █▀▀▀█ █── █ █▀▀█ █▄─ █"
+echo -e "▄─ █ █── █ █▀▀▀ █▀▄─ █▀▀▀ █▄▄▀ ▀▀▀▄▄ ─█ █─ █▄▄█ █ █ █"
+echo -e "█▄▄█ █▄▄▄█ █▄▄▄ █─ █ █▄▄▄ █─ █ █▄▄▄█ ─▀▄▀─ █─── █──▀█"
+echo -e "${RED}=====================================================${CLR}"
+echo
+
+apt-get -y install dnsutils # nslookup
+apt-get -y install tcpdump
+apt-get -y install dsniff # tcpkill
+apt-get -y install grepcidr
+
+wget -q https://github.com/jgmdev/ddos-deflate/archive/master.zip
+unzip master.zip
+rm master.zip
+cd ddos-deflate-master
+bash install.sh
+cd && rm -r ddos-deflate-master
+
+systemctl daemon-reload
+systemctl enable ddos
+
+# delete ddos-deflate script
+rm ~/ddosdef.sh
+
+echo
+echo -e "==========[${RED} CYBERTIZE SETUP SCRIPT V1.0.0 ${CLR}]=========="
+echo -e "${YELLOW} Name${CLR}:${GREEN} ddosdef.sh                                    ${CLR}"
+echo -e "${YELLOW} Desc${CLR}:${GREEN} Script to install ddos-deflate automatic      ${CLR}"
+echo -e "${YELLOW} Auth${CLR}:${GREEN} Doctype <cybertizedevel@gmail.com>            ${CLR}"
+echo -e "=====[${RED} CREATED BY DOCTYPE, ALL RIGHT RESERVED. ${CLR}]====="
+echo
---
+++ packages/nginx.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source /usr/local/cybertize/helper/color.sh
+source /usr/local/cybertize/helper/function.sh
+source /usr/local/cybertize/helper/utility.sh
+
+clear && echo
+echo -e "${RED}=====================================================${CLR}"
+echo -e "── █ █▀▀▀█ █▀▀▀ █─▄▀ █▀▀▀ █▀▀█ █▀▀▀█ █── █ █▀▀█ █▄─ █"
+echo -e "▄─ █ █── █ █▀▀▀ █▀▄─ █▀▀▀ █▄▄▀ ▀▀▀▄▄ ─█ █─ █▄▄█ █ █ █"
+echo -e "█▄▄█ █▄▄▄█ █▄▄▄ █─ █ █▄▄▄ █─ █ █▄▄▄█ ─▀▄▀─ █─── █──▀█"
+echo -e "${RED}=====================================================${CLR}"
+echo
+
+DOMAIN=$(grep -sw 'DOMAIN' /usr/local/cybertize/.environment | cut -d '=' -f 2 | tr -d '"')
+
+apt-get -y install nginx
+apt-get -y install certbot
+apt-get -y install python3-certbot-nginx
+systemctl stop nginx
+
+cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
+certbot --register-unsafely-without-email --agree-tos --non-interactive --nginx -d $DOMAIN -d www.$DOMAIN
+
+# delete nginx script
+rm -f ~/nginx.sh
+
+echo
+echo -e "==========[${RED} CYBERTIZE SETUP SCRIPT V1.0.0 ${CLR}]=========="
+echo -e "${YELLOW} Name${CLR}:${GREEN} nginx.sh                                      ${CLR}"
+echo -e "${YELLOW} Desc${CLR}:${GREEN} Script to install nginx automatic             ${CLR}"
+echo -e "${YELLOW} Auth${CLR}:${GREEN} Doctype <cybertizedevel@gmail.com>            ${CLR}"
+echo -e "=====[${RED} CREATED BY DOCTYPE, ALL RIGHT RESERVED. ${CLR}]====="
+echo
