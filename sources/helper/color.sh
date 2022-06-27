f063fb97582866b59077893b2462d215f1eb6b3b
Doctype <87948372+cybertize@users.noreply.github.com>
2022-06-28 02:29:45 +0800

    Add files via upload

---
+++ sources/helper/color.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+WHITE="\e[37m"
+CLR="\e[0m"
+
+ERROR="\e[37;41;1m"
+WARNING="\e[37;41;1m"
+SUCCESS="\e[37;41;1m"
+MESSAGE="\e[37;41;1m"
