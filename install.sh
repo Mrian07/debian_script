c9de5a8358681e181c007cc4b0f1265ae1faf2e8
Doctype <87948372+cybertize@users.noreply.github.com>
2022-10-27 05:07:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,17 +7,17 @@ WHITE="\e[37;1m"
 CLR="\e[0m"
 
 if [[ "$USER" != root ]]; then
-  echo -e "${RED}Skrip perlu dijalankan dengan root!${CLR}" && exit 1
+    echo -e "${RED}Skrip perlu dijalankan dengan root!${CLR}" && exit 1
 fi
 
 getID=$(grep -ws 'ID' /etc/os-release | cut -d '=' -f 2)
 if [[ $getID == "debian" ]]; then
-  getVersion=$(grep -s 'VERSION_ID' /etc/os-release | cut -d '=' -f 2 | tr -d '"')
-  if [[ $getVersion -ne 10 ]]; then
-    echo -e "${RED}Versi Debian anda tidak disokong!${CLR}" && exit 1
-  fi
+    getVersion=$(grep -s 'VERSION_ID' /etc/os-release | cut -d '=' -f 2 | tr -d '"')
+    if [[ $getVersion -ne 10 ]]; then
+        echo -e "${RED}Versi Debian anda tidak disokong!${CLR}" && exit 1
+    fi
 else
-  echo -e "${RED}Skrip hanya untuk Linux Debian sahaja!${CLR}" && exit 1
+    echo -e "${RED}Skrip hanya untuk Linux Debian sahaja!${CLR}" && exit 1
 fi
 
 timedatectl set-timezone Asia/Kuala_Lumpur
@@ -34,7 +34,7 @@ apt-get -y install vnstat speedtest-cli
 apt-get -y install uuid-runtime
 apt-get -y install rng-tools
 echo "HRNGDEVICE=/dev/urandom" >>/etc/default/rng-tools
-systemctl restart rng-tools &>/dev/null
+systemctl restart rng-tools
 
 echo "deb http://ftp.debian.org/debian buster-backports main
 deb-src http://ftp.debian.org/debian buster-backports main" >> /etc/apt/sources.list
@@ -76,7 +76,7 @@ DNS=76.76.2.117 76.76.2.118
 FallbackDNS=dns.google
 #Domains=cybertize.tk www.cybertize.tk
 #LLMNR=yes
-#MulticastDNS=yes
+MulticastDNS=yes
 #DNSSEC=allow-downgrade
 #DNSOverTLS=yes
 #Cache=yes
