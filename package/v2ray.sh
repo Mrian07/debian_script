1f81893d228cff57554a7e18c7f1d87dc565a4b1
Doctype <87948372+cybertize@users.noreply.github.com>
2022-11-07 13:56:04 +0800

    Update v2ray.sh

--- package/v2ray.sh
+++ package/v2ray.sh
@@ -20,15 +20,14 @@ if [[ ! -f /usr/local/etc/v2ray/accounts ]]; then
     touch /usr/local/etc/v2ray/accounts
 fi
 
-certbot certonly --register-unsafely-without-email --agree-tos --standalone -d v2ray.cybertize.tk --cert-name v2ray
+certbot certonly --register-unsafely-without-email --agree-tos --standalone -d v2ray.${DOMAIN} --cert-name v2ray
 cp /etc/letsencrypt/live/v2ray/fullchain.pem /usr/local/etc/v2ray/fullchain.crt
 cp /etc/letsencrypt/live/v2ray/privkey.pem /usr/local/etc/v2ray/private.key
 
 chmod 644 /usr/local/etc/v2ray/fullchain.crt
 chmod 644 /usr/local/etc/v2ray/private.key
 
-# [TROJAN] TCP-TLS
-cat >/usr/local/etc/v2ray/trojan-tcp-tls.json <<-TROJAN1
+cat >/usr/local/etc/v2ray/trojan.json <<-TROJAN
 {
     "log": {
         "loglevel": "warning"
@@ -68,10 +67,9 @@ cat >/usr/local/etc/v2ray/trojan-tcp-tls.json <<-TROJAN1
         }
     ]
 }
-TROJAN1
+TROJAN
 
-# [VLESS] TLS-GRPC
-cat >/usr/local/etc/v2ray/vless-grpc-tls.json <<-VLESS1
+cat >/usr/local/etc/v2ray/vless.json <<-VLESS
 {
     "log": {
         "loglevel": "warning"
@@ -79,7 +77,7 @@ cat >/usr/local/etc/v2ray/vless-grpc-tls.json <<-VLESS1
     "inbounds": [
         {
             "listen": "0.0.0.0",
-            "port": 5142,
+            "port": 443,
             "protocol": "vless",
             "settings": {
                 "clients": [
@@ -109,27 +107,28 @@ cat >/usr/local/etc/v2ray/vless-grpc-tls.json <<-VLESS1
                     "serviceName": "GunService"
                 }
             }
-        }
-    ],
-    "outbounds": [
+        },
         {
-            "protocol": "freedom",
-            "tag": "direct"
-        }
-    ]
-}
-VLESS1
-
-# [VLESS] TCP-TLS
-cat >/usr/local/etc/v2ray/vless-tcp-tls.json <<-VLESS2
-{
-    "log": {
-        "loglevel": "warning"
-    },
-    "inbounds": [
+            "protocol": "vless",
+            "port": 443,
+            "settings": {
+                "decryption":"none",
+                "clients": [
+                    {
+                        "id": "$UUID"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "kcp",
+                "kcpSettings": {
+                    "seed": "{{ seed }}"
+                }
+            }
+        },
         {
             "listen": "0.0.0.0",
-            "port": 5179,
+            "port": 443,
             "protocol": "vless",
             "settings": {
                 "clients": [
@@ -143,10 +142,34 @@ cat >/usr/local/etc/v2ray/vless-tcp-tls.json <<-VLESS2
                 "fallbacks": [
                     {
                         "dest": 8001
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp"
+            }
+        },
+        {
+            "port": 443,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "level": 0,
+                        "email": "$USER@$DOMAIN"
+                    }
+                ],
+                "decryption": "none",
+                "fallbacks": [
+                    {
+                        "dest": "/dev/shm/default.sock",
+                        "xver": 1
                     },
                     {
                         "alpn": "h2",
-                        "dest": 8002
+                        "dest": "/dev/shm/h2c.sock",
+                        "xver": 1
                     }
                 ]
             },
@@ -154,7 +177,6 @@ cat >/usr/local/etc/v2ray/vless-tcp-tls.json <<-VLESS2
                 "network": "tcp",
                 "security": "tls",
                 "tlsSettings": {
-                    "serverName": "$DOMAIN",
                     "alpn": [
                         "h2",
                         "http/1.1"
@@ -167,26 +189,91 @@ cat >/usr/local/etc/v2ray/vless-tcp-tls.json <<-VLESS2
                     ]
                 }
             }
-        }
-    ],
-    "outbounds": [
+        },
         {
-            "protocol": "freedom",
-            "tag": "direct"
-        }
-    ]
-}
-VLESS2
-
-# [VLESS] TCP-WS
-cat >/usr/local/etc/v2ray/vless-tcp-ws.json <<-VLESS3
-{
-    "log": {
-        "loglevel": "warning"
-    },
-    "inbounds": [
+            "listen": "0.0.0.0",
+            "port": 443,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "level": 0,
+                        "email": "$USER@$DOMAIN"
+                    }
+                ],
+                "decryption": "none",
+                "fallbacks": [
+                    {
+                        "dest": 8001,
+                        "xver": 1
+                    },
+                    {
+                        "alpn": "h2",
+                        "dest": 8002,
+                        "xver": 1
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "tls",
+                "tlsSettings": {
+                    "serverName": "$USER@$DOMAIN",
+                    "alpn": [
+                        "h2",
+                        "http/1.1"
+                    ],
+                    "certificates": [
+                        {
+                            "certificateFile": "/usr/local/etc/v2ray/fullchain.crt",
+                            "keyFile": "/usr/local/etc/v2ray/private.key"
+                        }
+                    ]
+                }
+            }
+        },
         {
-            "port": 5428,
+            "port": 443,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "level": 0,
+                        "email": "$USER@$DOMAIN"
+                    }
+                ],
+                "decryption": "none",
+                "fallbacks": [
+                    {
+                        "dest": 80
+                    },
+                    {
+                        "path": "/websocket",
+                        "dest": 8001,
+                        "xver": 1
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "tls",
+                "tlsSettings": {
+                    "alpn": [
+                        "http/1.1"
+                    ],
+                    "certificates": [
+                        {
+                            "certificateFile": "/usr/local/etc/v2ray/fullchain.crt",
+                            "keyFile": "/usr/local/etc/v2ray/private.key"
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            "port": 443,
             "listen": "127.0.0.1",
             "protocol": "vless",
             "settings": {
@@ -212,28 +299,16 @@ cat >/usr/local/etc/v2ray/vless-tcp-ws.json <<-VLESS3
     "outbounds": [
         {
             "protocol": "freedom"
+        },
+        {
+            "protocol": "freedom",
+            "tag": "direct"
         }
     ]
-}
-VLESS3
 
-cat >/etc/nginx/conf.d/vless-tcp-tls.conf <<-NGINX
-server {
-    listen 127.0.0.1:8001;
-    listen 127.0.0.1:8002 http2;
-    server_name $DOMAIN www.$DOMAIN;
-    charset utf-8;
+VLESS
 
-    location / {
-        root /usr/share/nginx/html;
-    }
-    error_page  404 /404.html;
-    error_page 500 502 503 504 /50x.html;
-}
-NGINX
-
-# [VMESS] TLS-HTTP2
-cat >/usr/local/etc/v2ray/vmess-http-tls.json <<-VMESS1
+cat >/usr/local/etc/v2ray/vmess.json <<-VMESS
 {
     "log": {
         "loglevel": "warning"
@@ -253,7 +328,49 @@ cat >/usr/local/etc/v2ray/vmess-http-tls.json <<-VMESS1
     "inbounds": [
         {
             "listen": "0.0.0.0",
-            "port": 6045,
+            "port": 1234,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "tcpSettings": {
+                    "header": {
+                        "type": "http",
+                        "response": {
+                            "version": "1.1",
+                            "status": "200",
+                            "reason": "OK",
+                            "headers": {
+                                "Content-Type": [
+                                    "application/octet-stream",
+                                    "video/mpeg",
+                                    "application/x-msdownload",
+                                    "text/html",
+                                    "application/x-shockwave-flash"
+                                ],
+                                "Transfer-Encoding": [
+                                    "chunked"
+                                ],
+                                "Connection": [
+                                    "keep-alive"
+                                ],
+                                "Pragma": "no-cache"
+                            }
+                        }
+                    }
+                },
+                "security": "none"
+            }
+        },
+        {
+            "listen": "0.0.0.0",
+            "port": 1234,
             "protocol": "vmess",
             "settings": {
                 "clients": [
@@ -274,43 +391,42 @@ cat >/usr/local/etc/v2ray/vmess-http-tls.json <<-VMESS1
                     ]
                 }
             }
-        }
-    ],
-    "outbounds": [
-        {
-            "protocol": "freedom",
-            "tag": "direct"
         },
         {
-            "protocol": "blackhole",
-            "tag": "block"
-        }
-    ]
-}
-VMESS1
-
-# [VMESS] TCP-TLS
-cat >/usr/local/etc/v2ray/vmess-tcp-tls.json <<-VMESS2
-{
-    "log": {
-        "loglevel": "warning"
-    },
-    "routing": {
-        "domainStrategy": "AsIs",
-        "rules": [
-            {
-                "type": "field",
-                "ip": [
-                    "geoip:private"
-                ],
-                "outboundTag": "block"
+            "protocol": "vmess",
+            "port": 1234,
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "kcp",
+                "kcpSettings": {
+                    "seed": "{{ seed }}"
+                }
             }
-        ]
-    },
-    "inbounds": [
+        },
         {
             "listen": "0.0.0.0",
-            "port": 6137,
+            "port": 1234,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp"
+            }
+        },
+        {
+            "listen": "0.0.0.0",
+            "port": 1234,
             "protocol": "vmess",
             "settings": {
                 "clients": [
@@ -331,43 +447,26 @@ cat >/usr/local/etc/v2ray/vmess-tcp-tls.json <<-VMESS2
                     ]
                 }
             }
-        }
-    ],
-    "outbounds": [
-        {
-            "protocol": "freedom",
-            "tag": "direct"
         },
         {
-            "protocol": "blackhole",
-            "tag": "block"
-        }
-    ]
-}
-VMESS2
-
-# [VMESS] TCP-TLS-WS
-cat >/usr/local/etc/v2ray/vmess-tls-ws.json <<-VMESS3
-{
-    "log": {
-        "loglevel": "warning"
-    },
-    "routing": {
-        "domainStrategy": "AsIs",
-        "rules": [
-            {
-                "type": "field",
-                "ip": [
-                    "geoip:private"
-                ],
-                "outboundTag": "block"
+            "listen": "0.0.0.0",
+            "port": 1234,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none"
             }
-        ]
-    },
-    "inbounds": [
+        },
         {
             "listen": "0.0.0.0",
-            "port": 6273,
+            "port": 1234,
             "protocol": "vmess",
             "settings": {
                 "clients": [
@@ -392,6 +491,9 @@ cat >/usr/local/etc/v2ray/vmess-tls-ws.json <<-VMESS3
     ],
     "outbounds": [
         {
+            "protocol": "freedom"
+        },
+        {
             "protocol": "freedom",
             "tag": "direct"
         },
@@ -401,15 +503,63 @@ cat >/usr/local/etc/v2ray/vmess-tls-ws.json <<-VMESS3
         }
     ]
 }
-VMESS3
+VMESS
 
-systemctl enable v2ray@trojan-tcp-tls
-systemctl enable v2ray@vless-grpc-tls
-systemctl enable v2ray@vless-tcp-tls
-systemctl enable v2ray@vless-tcp-ws
-systemctl enable v2ray@vmess-http-tls
-systemctl enable v2ray@vmess-tcp-tls
-systemctl enable v2ray@vmess-tls-ws
+cat >/etc/nginx/conf.d/vless.conf <<-NGINX
+server {
+    listen 127.0.0.1:8001;
+    #listen 127.0.0.1:8002 http2;
+    server_name $DOMAIN www.$DOMAIN;
+    charset utf-8;
+
+    location / {
+        root /usr/share/nginx/html;
+    }
+    error_page 404 /404.html;
+    error_page 500 502 503 504 /50x.html;
+}
+
+server {
+    listen 127.0.0.1:8001 proxy_protocol;
+    listen 127.0.0.1:8002 http2 proxy_protocol;
+    server_name $DOMAIN www.$DOMAIN;
+
+    set_real_ip_from 127.0.0.1;
+    charset utf-8;
+    #access_log logs/yourserver.access.log proxy;
+
+    location / {
+        root /usr/share/nginx/html;
+    }
+
+    error_page 404 /404.html;
+    error_page 500 502 503 504 /50x.html;
+}
+
+server {
+    listen unix:/dev/shm/default.sock proxy_protocol;
+    listen unix:/dev/shm/h2c.sock http2 proxy_protocol;
+    server_name _;
+    root root /usr/share/nginx/html;
+    set_real_ip_from 127.0.0.1;
+    include /etc/nginx/default.d/*.conf;
+
+    location / {
+    }
+
+    error_page 404 /404.html;
+        location = /40x.html {
+    }
+
+    error_page 500 502 503 504 /50x.html;
+        location = /50x.html {
+    }
+}
+NGINX
+
+systemctl enable v2ray@trojan
+systemctl enable v2ray@vless
+systemctl enable v2ray@vmess
 
 rm -f ~/v2ray.sh
 echo "====================================================="
